40c8e15334b227b9d6fb320b31de8c493e551cce
Oleksii Stepanyk <ostepanyk@kse.org.ua>
2024-07-15 16:13:08 +0300

    added script and fixed bugs

--- Interpreter/interpeter.cpp
+++ Interpreter/interpeter.cpp
@@ -10,34 +10,32 @@ class Tokenizer {
 public:
 	vector<string> Tokenize(string input) {
 		vector<string> tokens = {};
-		remove(input.begin(), input.end(), ' ');
+		input.erase(remove(input.begin(), input.end(), ' '), input.end());
 
 		regex digitRegex("\\d");
 		regex tokensRegex("[\\(\\)]");
 		regex operatorRegex("[\\+\\-\\*\\/]");
-		regex functionRegex("(pow)|(abs)|(min)|(max)");
+		regex functionRegex("(pow|abs|min|max)");
 
 		int rightParenthesesCount = 0;
 		int leftParenthesesCount = 0;
 
-		for (int index = 0; index < input.size(); index++)
-		{
+		for (int index = 0; index < input.size(); index++) {
 			string currentChar(1, input[index]);
 			string temp;
 			if (regex_match(currentChar, digitRegex) ||
-				(currentChar == "-" && (index == 0 || regex_match(temp = (1, input[index - 1]), operatorRegex))))
-			{
+				(currentChar == "-" && (index == 0 ||
+					regex_match(temp = string(1, input[index - 1]), operatorRegex) ||
+					regex_match(temp = string(1, input[index - 1]), tokensRegex)))) {
 				string number = currentChar;
-				while (index + 1 < input.size() && (regex_match(temp = (1, input[index + 1]), digitRegex)))
-				{
-					temp = (1, input[++index]);
+				while (index + 1 < input.size() && (regex_match(temp = string(1, input[index + 1]), digitRegex))) {
+					temp = string(1, input[++index]);
 					number += temp;
 				}
 
 				tokens.push_back(number);
 			}
-			else if (regex_match(currentChar, tokensRegex) || regex_match(currentChar, operatorRegex))
-			{
+			else if (regex_match(currentChar, tokensRegex) || regex_match(currentChar, operatorRegex)) {
 				if (currentChar == "(") {
 					if (input[index + 1] == ')') throw runtime_error("Parentheses are empty or wrong written");
 					leftParenthesesCount++;
@@ -47,27 +45,25 @@ public:
 				}
 				tokens.push_back(currentChar);
 			}
-			else if (index + 2 < input.size() && regex_match((currentChar + input[index + 1] + input[index + 2]), functionRegex))
-			{
+			else if (index + 2 < input.size() && regex_match(input.substr(index, 3), functionRegex)) {
 				int leftParenthesesIndex = index + 3;
 				if (input[leftParenthesesIndex] != '(') throw runtime_error("Values for cos/sin functions must be in parentheses");
-				string function = currentChar + input[index + 1] + input[index + 2];
+				string function = input.substr(index, 3);
 				index += 2;
 				tokens.push_back(function);
 			}
 		}
-		if (rightParenthesesCount != leftParenthesesCount)
-		{
+		if (rightParenthesesCount != leftParenthesesCount) {
 			throw runtime_error("Some parentheses are missing");
 		}
 		return tokens;
 	}
 };
 
+
 class ShuntingYard {
 private:
-	int Precedence(string op)
-	{
+	int Precedence(string op) {
 		if (op == "max" || op == "min") return 1;
 		if (op == "+" || op == "-") return 2;
 		if (op == "*" || op == "/") return 3;
@@ -76,8 +72,7 @@ private:
 		return 0;
 	}
 
-	bool IsLeftAssociative(string op)
-	{
+	bool IsLeftAssociative(string op) {
 		if (op == "+" || op == "-" || op == "*" || op == "/") return true;
 		return false;
 	}
@@ -86,33 +81,25 @@ public:
 	queue<string> toRPN(vector<string> tokens) {
 		regex numberRegex("^-?[0-9]+$");
 		regex operatorRegex("[\\+\\-\\*\\/]");
-		regex functionRegex("(pow)|(abs)|(min)|(max)");
+		regex functionRegex("(pow|abs|min|max)");
 
 		queue<string> outputQueue = {};
 		stack<string> operatorStack = {};
 
-		for (string& token : tokens)
-		{
-			if (regex_match(token, numberRegex))
-			{
+		for (int i = 0; i < tokens.size(); i++) {
+			string token = tokens[i];
+			if (regex_match(token, numberRegex)) {
 				outputQueue.push(token);
 			}
-
-			else if (regex_match(token, functionRegex))
-			{
+			else if (regex_match(token, functionRegex)) {
 				operatorStack.push(token);
 			}
-			else if (regex_match(token, operatorRegex))
-			{
-				if (!operatorStack.empty()) {
-
-					while (operatorStack.top() != ")" &&
-						(Precedence(operatorStack.top()) > Precedence(token) ||
-							Precedence(operatorStack.top()) == Precedence(token) && IsLeftAssociative(token)))
-					{
-						outputQueue.push(operatorStack.top());
-						operatorStack.pop();
-					}
+			else if (regex_match(token, operatorRegex)) {
+				while (!operatorStack.empty() && operatorStack.top() != "(" &&
+					(Precedence(operatorStack.top()) > Precedence(token) ||
+						(Precedence(operatorStack.top()) == Precedence(token) && IsLeftAssociative(token)))) {
+					outputQueue.push(operatorStack.top());
+					operatorStack.pop();
 				}
 				operatorStack.push(token);
 			}
@@ -120,17 +107,18 @@ public:
 				operatorStack.push(token);
 			}
 			else if (token == ")") {
-				while (operatorStack.top() != "(")
-				{
+				while (operatorStack.top() != "(") {
 					outputQueue.push(operatorStack.top());
 					operatorStack.pop();
 				}
 				operatorStack.pop();
-				break;
+				if (!operatorStack.empty() && regex_match(operatorStack.top(), functionRegex)) {
+					outputQueue.push(operatorStack.top());
+					operatorStack.pop();
+				}
 			}
 		}
-		while (!operatorStack.empty())
-		{
+		while (!operatorStack.empty()) {
 			outputQueue.push(operatorStack.top());
 			operatorStack.pop();
 		}
@@ -138,17 +126,16 @@ public:
 	}
 };
 
+
 class Calculator {
 private:
-	int ApplyOperation(int firstOperand, int secondOperand, string op)
-	{
+	int ApplyOperation(int firstOperand, int secondOperand, string op) {
 		if (op == "+") return firstOperand + secondOperand;
 		if (op == "-") return firstOperand - secondOperand;
 		if (op == "*") return firstOperand * secondOperand;
 		if (op == "/") {
-			if (secondOperand == 0)
-			{
-				throw new runtime_error("Cannot be divided by zero.");
+			if (secondOperand == 0) {
+				throw runtime_error("Cannot be divided by zero.");
 			}
 			return firstOperand / secondOperand;
 		}
@@ -160,32 +147,27 @@ private:
 	}
 
 public:
-	int Calculate(queue<string> rpnQueue)
-	{
+	int Calculate(queue<string> rpnQueue) {
 		regex numberRegex("^-?[0-9]+$");
 		regex operatorRegex("[\\+\\-\\*\\/]");
 		stack<int> resultStack = {};
 
-		for (int count = rpnQueue.size(); count > 0; count--)
-		{
+		while (!rpnQueue.empty()) {
 			string token = rpnQueue.front();
 			rpnQueue.pop();
 
-			if (regex_match(token, numberRegex))
-			{
+			if (regex_match(token, numberRegex)) {
 				int number = stoi(token);
 				resultStack.push(number);
 			}
-			else if (token == "abs")
-			{
+			else if (token == "abs") {
 				int firstOperand = resultStack.top();
 				resultStack.pop();
 
 				int result = ApplyOperation(firstOperand, 0, token);
 				resultStack.push(result);
 			}
-			else if ((token == "max" || token == "min" || token == "pow") || (regex_match(token, operatorRegex) && resultStack.size() >= 2))
-			{
+			else if (token == "max" || token == "min" || token == "pow") {
 				int secondOperand = resultStack.top();
 				resultStack.pop();
 
@@ -195,8 +177,17 @@ public:
 				int result = ApplyOperation(firstOperand, secondOperand, token);
 				resultStack.push(result);
 			}
-			else
-			{
+			else if (regex_match(token, operatorRegex) && resultStack.size() >= 2) {
+				int secondOperand = resultStack.top();
+				resultStack.pop();
+
+				int firstOperand = resultStack.top();
+				resultStack.pop();
+
+				int result = ApplyOperation(firstOperand, secondOperand, token);
+				resultStack.push(result);
+			}
+			else {
 				cout << "Invalid token or insufficient operands for operation: " << token << endl;
 				exit(1);
 			}
@@ -206,28 +197,22 @@ public:
 	}
 };
 
-int main()
-{
+
+void processInput(string input) {
 	Tokenizer tokenizer;
 	ShuntingYard shuntingYard;
 	Calculator calculator;
+
+	vector<string> tokens = tokenizer.Tokenize(input);
+	queue<string> rpn = shuntingYard.toRPN(tokens);
+	int result = calculator.Calculate(rpn);
+
+	cout << result << endl;
+}
+
+int main() {
 	string input;
 	getline(cin, input);
-	vector<string> tokens = tokenizer.Tokenize(input);
-	for (string token : tokens)
-	{
-		cout << token << " ";
-	}
-	cout << endl;
-	queue<string> rpn = shuntingYard.toRPN(tokens);
-	queue<string> rpnCopy = rpn;
-	while (!rpnCopy.empty())
-	{
-		cout << rpnCopy.front() << " ";
-		rpnCopy.pop();
-	}
-	cout << endl;
-	int result = calculator.Calculate(rpn);
-	cout << result << endl;
+	processInput(input);
 	return 0;
 }
--- Script/Script.py
+++ Script/Script.py
@@ -8,9 +8,11 @@ def access_folder():
     os.chdir("x64")
     os.chdir("Debug")
 
+
 def run_process(input_data):
     process = subprocess.Popen(
         ["Interpreter.exe"],
+        stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True)
@@ -18,12 +20,79 @@ def run_process(input_data):
     return stdout.strip(), stderr.strip()
 
 
-def show_result():
-    output, error = run_process()
-    print(output)
+def test():
+    tests = [
+        {
+            "input": "7 * 2 + 3\n",
+            "expected_output": "17"
+        },
+        {
+            "input": "abs(-5) + abs(-2)\n",
+            "expected_output": "7"
+        },
+        {
+            "input": "max(4, min(5, 6))\n",
+            "expected_output": "5"
+        },
+        {
+            "input": "min(max(2, 3), max(4, 5))\n",
+            "expected_output": "3"
+        },
+        {
+            "input": "abs(3 - 6) + abs(2 - 5)\n",
+            "expected_output": "6"
+        },
+        {
+            "input": "5 + 3 * 2\n",
+            "expected_output": "11"
+        },
+        {
+            "input": "max(5, 2)\n",
+            "expected_output": "5"
+        },
+        {
+            "input": "min(3, 4)\n",
+            "expected_output": "3"
+        },
+        {
+            "input": "max(min(2 * 3, 2), 2)\n",
+            "expected_output": "2"
+        },
+        {
+            "input": "5 - abs(-3)\n",
+            "expected_output": "2"
+        },
+        {
+            "input": "5 + abs(-3)\n",
+            "expected_output": "8"
+        },
+        {
+            "input": "pow(3,5)\n",
+            "expected_output": "243"
+        },
+        {
+            "input": "pow(8, 3)\n",
+            "expected_output": "512"
+        }
+    ]
+
+    for test in tests:
+        input_data = test["input"]
+        expected_output = test["expected_output"]
+
+        output, error = run_process(input_data)
+
+        if error:
+            print(f"Test failed for input:\n{input_data}")
+            print(f"Error:\n{error}")
+        elif output.strip() != expected_output:
+            print(f"Test failed for input:\n{input_data}")
+            print(f"Expected:\n{expected_output}")
+            print(f"Got:\n{output}")
+        else:
+            print(f"Test passed for input:\n{input_data}")
 
 
 if __name__ == "__main__":
     access_folder()
-    while 1:
-        show_result()
+    test()
